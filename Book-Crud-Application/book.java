90b7a0ada7013097d44c564d8f362d85394ab992
bhargav-kumar-diwakar <bhargavkumardiwakar@gmail.com>
2026-02-06 18:00:11 +0530

    Added new file

---
+++ Book-Crud-Application/book.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class Book{
+    int bookId;
+    String title;
+    String author;
+    int price;
+
+    Book(int bookId, String title, String author, int price){
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
+
+
+public class book{
+    public static void main(String[] args){
+        System.out.println("---Options---");
+    }
+}
